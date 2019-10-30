5042e3c3ef291eda42f35fe2e306eb997b638aac
Isaias Vera <osvaldoisaias.spk@gmail.com>
2019-10-30 10:34:39 +0100

    New folder POO created, moved ColaException there

--- ColaException/src/com/isaiasvera/ColaVaciaException.java
+++
@@ -1,8 +0,0 @@
-package com.isaiasvera;
-
-public class ColaVaciaException extends Exception{
-    public ColaVaciaException(String message){
-        super(message);
-        System.out.println("La cola está vacía");
-    }
-}
